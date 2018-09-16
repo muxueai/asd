c22c0c31e5a3ea7aa72177a83f0869eae1074e54
soft01 <soft01@server-176.53.6.109.as42926.net>
2018-09-16 17:48:02 +0800

    ghhg

---
+++ src/jsd1807/DataTypeDemo.java
@@ -0,0 +1,132 @@
+package jsd1807;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		
+	
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*int a =5;
+		long b =a;
+		int c =(int)b;
+		long d =10000000000L;
+		int e =(int)d;
+		System.out.println(e);
+		float f =36.987F;
+		int g =(int)f;
+		System.out.println(g);
+		short s1 =6;
+		short s2 =8;
+		short s3 =(short)(s1+s2);
+		System.out.println(0+0);//0
+		System.out.println('0'+'0');//96
+		*/
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*int a =5;
+		int b =(int)5.0;
+		char c =97;
+		double d =26.975;
+		int i =(int)d;
+		System.out.println(i);
+		long g =356;
+		float h =g;
+		System.out.println(h);
+		float p =69.36f;
+		//double o =p;
+		System.out.println(p);*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*boolean b1 = true;
+		boolean b2 =false;
+		//boolean b3 =36.5;
+		char c1 ='男';
+		char c2 ='m';
+		char c3 =' ';
+		//char c4 ='';
+		//char c6='男性'；
+		char c7 = 98;
+		System.out.println(c7);//b
+		char c8 ='\'';
+		System.out.println(c8);*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
